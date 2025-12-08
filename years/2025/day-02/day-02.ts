fd2e0739407f14d0c53a3aa085ac6eff26204233
Marlo Swora <swora@uni-hildesheim.de>
2025-12-08 01:32:47 +0100

    add 2025/day-02 part2

--- years/2025/day-02/day-02.ts
+++ years/2025/day-02/day-02.ts
@@ -4,18 +4,24 @@ function main() {
 	console.log("Startign Solution for Day 02 of Advant of Code!");
 	const ranges = getRanges("years/2025/day-02/puzzle-input.txt");
 	let invalidsSummed = 0;
+	let invalidId2Summed = 0;
 	for (const r of ranges) {
 		const [startStr, endStr] = r.split("-");
 		let start = parseInt(startStr);
 		let end = parseInt(endStr);
-		const invalidIDs = calcInvalidIDs(start, end);
+		const { invalidIDs, invalidID2s } = calcInvalidIDs(start, end);
 		for (const invalidID of invalidIDs) {
 			invalidsSummed += invalidID;
 		}
+		for (const invalidID2 of invalidID2s) {
+			invalidId2Summed += invalidID2;
+		}
 
 		console.log(`Start:${start}, End:${end}, InvalidIDs:${invalidIDs}`);
+		console.log(`Invalid ID2s: ${invalidID2s}`);
 	}
 	console.log(`Invalid IDs summed: ${invalidsSummed}`);
+	console.log(`Invalid ID2s summed: ${invalidId2Summed}`);
 }
 
 function getRanges(filePath: string) {
@@ -26,13 +32,17 @@ function getRanges(filePath: string) {
 
 export function calcInvalidIDs(start: number, end: number) {
 	let invalidIDs = [];
+	let invalidID2s = [];
 	for (let id = start; id <= end; id++) {
-		const idValid = checkIdValid(id.toString());
-		if (!idValid) {
+		if (!checkIdValid(id.toString())) {
 			invalidIDs.push(id);
 		}
+
+		if (!checkIdValidV2(id.toString())) {
+			invalidID2s.push(id);
+		}
 	}
-	return invalidIDs;
+	return { invalidIDs, invalidID2s };
 }
 
 function checkIdValid(id: string) {
@@ -46,5 +56,45 @@ function checkIdValid(id: string) {
 		return true;
 	}
 }
+
+function checkIdValidV2(id: string): boolean {
+	for (let i = 1; i <= id.length / 2; i++) {
+		if (substringRepeats(id, i)) {
+			return false;
+		}
+	}
+	return true;
+}
+
+function substringRepeats(id: string, substringLength: number) {
+	// if the length of the subset does not fit into the id it can not repeat.
+	if (id.length % substringLength != 0) {
+		return false;
+	}
+
+	const splitId = createSubstrings(id, substringLength);
+	return checkItemsEqual(splitId);
+}
+
+function createSubstrings(originalString: string, substringLength: number) {
+	let substrings = [];
+	for (let i = 0; i < originalString.length; i += substringLength) {
+		substrings.push(originalString.slice(i, i + substringLength));
+	}
+	return substrings;
+}
+
+function checkItemsEqual(array: any[]) {
+	if (array.length < 2) {
+		return true;
+	}
+	for (let i = 0; i < array.length - 1; i++) {
+		if (array[i] !== array[i + 1]) {
+			return false;
+		}
+	}
+	return true;
+}
+
 
 main();
